1adc554f9c78767f7acb43b72f13916e23f4f07b
bram <en4abraham@gmail.com>
2025-03-27 13:05:14 +0100

    Deployed VeltToken.sol

--- smart-contract/ignition/modules/VeltUSDToken.js
+++
@@ -1,7 +0,0 @@
-const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
-
-module.exports = buildModule("LockModule", (m) => {
-  const lock = m.contract("Lock", [], {});
-
-  return { lock };
-});
