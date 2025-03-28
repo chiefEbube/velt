f9a9f4f36976a336b374468981f53c306d178747
bram <en4abraham@gmail.com>
2025-03-28 14:39:57 +0100

    Modified VLT to add a burn function && Added LendingPool.sol

--- smart-contract/contracts/LendingPool.sol
+++ smart-contract/contracts/LendingPool.sol
@@ -1,6 +1,42 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.23;
+pragma solidity ^0.8.19;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {VeltToken} from "./tokens/VeltToken.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 
-contract LendingPool {}
+contract LendingPool is Ownable, ReentrancyGuard {
+    VeltToken public vltToken;
+    mapping(address => uint256) public deposits;
+
+    event Deposited(address indexed user, uint256 amount);
+    event Withdrawn(address indexed user, uint256 amount);
+
+    constructor(address _vltToken) Ownable(msg.sender) {
+        vltToken = VeltToken(_vltToken);
+    }
+
+    function deposit() external payable {
+        require(msg.value > 0, "Deposit must be greater than zero");
+
+        deposits[msg.sender] += msg.value;
+        vltToken.mint(msg.sender, msg.value);
+
+        emit Deposited(msg.sender, msg.value);
+    }
+
+    function withdraw(uint256 amount) external nonReentrant {
+        require(deposits[msg.sender] >= amount, "Insufficient balance");
+
+        deposits[msg.sender] -= amount;
+
+        (bool success, ) = msg.sender.call{value: amount}("");
+        require(success, "ETH Transfer failed");
+
+        vltToken.burn(msg.sender, amount);
+
+        emit Withdrawn(msg.sender, amount);
+    }
+
+    receive() external payable {}
+}
--- smart-contract/contracts/tokens/VeltToken.sol
+++ smart-contract/contracts/tokens/VeltToken.sol
@@ -4,18 +4,28 @@ pragma solidity ^0.8.23;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import {ERC20Burnable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 /// @custom:security-contact en4abraham@gmail.com
-contract VeltToken is ERC20, Ownable, ERC20Permit {
+
+contract VeltToken is ERC20, Ownable, ERC20Burnable, ERC20Permit {
     constructor(
         address recipient,
         address initialOwner
-    ) ERC20("VeltToken", "VLT") Ownable(initialOwner) ERC20Permit("VeltToken") {
+    )
+        ERC20("Velt Token", "VLT")
+        Ownable(initialOwner)
+        ERC20Permit("VeltToken")
+    {
         _mint(recipient, 100000 * 10 ** decimals());
     }
 
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
+    }
+
+    function burn(address from, uint256 amount) public onlyOwner {
+        _burn(from, amount);
     }
 }
